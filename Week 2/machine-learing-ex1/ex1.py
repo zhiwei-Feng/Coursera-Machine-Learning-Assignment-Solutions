2b8699cd3390751b7304064d4840db004e276999
Adrian <zhiwei.feng1995@gmail.com>
2019-06-15 14:10:51 +0800

    started machine-learning-ex1 complete warmExercise

---
+++ Week 2/machine-learing-ex1/ex1.py
@@ -0,0 +1,24 @@
+import warmUpExercise
+import os
+import pandas as pd
+import numpy as np
+
+# ==================== Part 1: Basic Function ====================
+# Complete warmUpExercise.py
+print('Running warmUpExercise ... ')
+print('5x5 Identity Matrix: ')
+print(warmUpExercise.warm_up_exercise())
+print('Program paused. Press enter to continue.')
+os.system('pause')
+
+# ======================= Part 2: Plotting =======================
+print('Plotting Data ...')
+data = np.loadtxt('ex1data1.txt', delimiter=',')
+X = data[:, 0]
+y = data[:, 1]
+m = len(y) # number of training examples
+
+# plot data
+# Note: You have to complete the code in plotData.py
+
+
---
+++ Week 2/machine-learing-ex1/warmUpExercise.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def warm_up_exercise():
+    # A = warm_up_exercise() is an example function that returns the 5x5 identity matrix
+    # ============= YOUR CODE HERE ==============
+    # Instructions: Return the 5x5 identity matrix
+    #               In octave, we return values by defining which variables
+    #               represent the return values (at the top of the file)
+    #               and then set them accordingly.
+    A = np.eye(5)
+    return A
+    # ===========================================
